dd3b4d914d94f61e8e30e84f342f5a08aa7d53b5
isaberganton <isadora.berganton.dev2025@gmail.com>
2025-05-15 15:53:22 -0300

    updated

--- src/models/User.js
+++ src/models/User.js
@@ -5,11 +5,6 @@ const UserSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user",
-    },
   },
   { timestamps: true }
 );
